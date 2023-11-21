e69a88d5cac1000b6824498437e7124b016d9847
Andres Diaz Popov <elgalledesarrollo@gmail.com>
2023-11-21 13:54:07 +0100

    Changes to add or remove products from the cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
 
   def show
+    @carts_products = @cart.carts_products
   end
 
   def add_product
@@ -8,9 +9,17 @@ class CartsController < ApplicationController
     number_products_cart = cart.carts_products.sum(:quantity)
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("number_products_cart",
-                                                  partial: 'carts/number_products_cart',
-                                                  locals: { number_products_cart: number_products_cart})
+        if params[:product_added_from_cart] == 'true'
+          render turbo_stream: [
+            turbo_stream.replace("number_products_cart", partial: 'carts/number_products_cart',
+                                 locals: { number_products_cart: number_products_cart, open_modal: false}),
+            turbo_stream.replace("products_cart_modal", partial: 'carts/products_cart_modal',
+                                 locals: { carts_products: cart.carts_products, open_modal: false})
+          ]
+        else
+          render turbo_stream: turbo_stream.replace("number_products_cart", partial: 'carts/number_products_cart',
+                                 locals: { number_products_cart: number_products_cart, open_modal: false})
+        end
       end
     end
 
